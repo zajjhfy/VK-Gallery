bab9203d0e81cd880a614970a7164dab237962c4
zajjhfy <zajjhfy@gmail.com>
2025-10-02 17:15:41 +0300

    Initial Commit

---
+++ VK Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK Gallery
+//
+//  Created by Иван Коновалов on 02.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
